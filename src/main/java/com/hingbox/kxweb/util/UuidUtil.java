48b2d7377de2fae17d706d3dbbe0842c5769640c
hingbox <hingbox@163.com>
2017-04-28 20:55:27 +0800

    初始提交

---
+++ src/main/java/com/hingbox/kxweb/util/UuidUtil.java
@@ -0,0 +1,16 @@
+package com.hingbox.kxweb.util;
+
+import java.util.UUID;
+
+public class UuidUtil {
+
+	public static String get32UUID() {
+		String uuid = UUID.randomUUID().toString().trim().replaceAll("-", "");
+		return uuid;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(get32UUID());
+	}
+}
+
